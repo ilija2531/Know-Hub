5f570a9f2e039a812d8701da15dfcbb1ea0866b9
ilija2531 <bogdanovskiilija18@gmail.com>
2024-12-18 18:06:12 +0100

    updated repository

--- KnowHubApp.client/src/App.jsx
+++ KnowHubApp.client/src/App.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Home from "./pages/Home/Home.jsx";
-import MyLearning from "./pages/MyLearning/MyLearning.jsx";
+import MyCourses from "./pages/MyCourses/MyCourses.jsx";
 import Catalog from "./pages/Catalog/Catalog.jsx";
-import Favorites from "./pages/Favorites/Favorites.jsx";
+import MyProfile from "./pages/MyProfile/MyProfile.jsx";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-
 
     const current_theme = localStorage.getItem('current_theme');
     const [theme, setTheme] = useState(current_theme ? current_theme :'light');
@@ -22,9 +21,9 @@ function App() {
         <Navbar theme={theme} setTheme={setTheme}/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/myLearning" element={<MyLearning/>}/>
+        <Route path="/myCourses" element={<MyCourses/>}/>
         <Route path="/catalog" element={<Catalog/>}/>
-        <Route path="/favorites" element={<Favorites/>}/>
+        <Route path="/myProfile" element={<MyProfile/>}/>
       </Routes>
       </div>
       </>
--- KnowHubApp.client/src/Components/Navbar/Navbar.jsx
+++ KnowHubApp.client/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,36 @@
 import { Link} from 'react-router-dom'
+import React, {useState} from 'react'
 import './Navbar.css'
 import search_icon from '../../assets/Navbar-sliki/search-1.png'
 import search_icon_white from '../../assets/Navbar-sliki/search-2.png'
 import light_mode from '../../assets/Navbar-sliki/day.png'
 import dark_mode from '../../assets/Navbar-sliki/night.png'
 import logo_img from '../../assets/Navbar-sliki/logo1.png'
+import account_img from '../../assets/Navbar-sliki/account.png'
+import home_img from '../../assets/Navbar-sliki/home.png'
+import catalog_img from '../../assets/Navbar-sliki/catalog.png'
+import courses_img from '../../assets/Navbar-sliki/myCourses.png'
+import logout_img from '../../assets/Navbar-sliki/logout.png'
+
+
+
 
 const Navbar = ({theme, setTheme}) =>{
+
+    const [open, setOpen] = useState(false);
 
     const toggle_mode = () => {
         theme == 'light' ?  setTheme('dark') : setTheme('light');
     }
 
+    function DropdownItem(props){
+        return(
+            <li className='dropdownItem'>
+                <img src={props.img} alt={props.text}></img>
+                <a>{props.text}</a>
+            </li>
+        )
+    }
    
         
     return(
@@ -20,20 +39,29 @@ const Navbar = ({theme, setTheme}) =>{
 
             <img src={theme == 'light' ? logo_img : logo_img} className='logoImg'></img>
 
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/myLearning">My Learning</Link></li>
-                <li><Link to="/catalog">Catalog</Link></li>
-                <li><Link to="/favorites">Favorites</Link></li>
-            </ul>
+            
 
             <div className='search-box'>
                 <input type='text' placeholder='Search'/>
                 <img src={theme == 'light' ? search_icon : search_icon} alt=''></img>
             
             </div>
+            <div className='dropdown'>
+                <div className='dropdown-trigger' onClick={()=>{setOpen(!open)}}>
+                <img className='account-img' src={account_img}></img>
+            </div>    
+            <div className={`dropdown-menu ${open ? 'active' : 'inactive'}`}>
+                <h3>Nikola Gruevski</h3>
+                <ul>
+                <Link to='/'><DropdownItem img = {home_img} text = {"Home"}/></Link>
+                <Link to='/myProfile'><DropdownItem img = {account_img} text = {"My Profile"}/></Link>
+                <Link to='/catalog'><DropdownItem img = {catalog_img} text = {"Catalog"}/></Link>
+                <Link to='/myCourses'><DropdownItem img = {courses_img} text = {"My Courses"}/></Link>
+                <Link to='/logout'><DropdownItem img = {logout_img} text = {"Logout"}/></Link>    
+                </ul>
             <img className='toggle'onClick={()=>{toggle_mode()}} src={theme == 'light' ? dark_mode : light_mode} alt='toggle-icon'></img>
-
+            </div>
+            </div>
             
         </div>
         <hr></hr>
