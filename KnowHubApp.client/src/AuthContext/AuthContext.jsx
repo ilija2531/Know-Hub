9759df751ab9f7ab93c9737d3858ad96b6ca71f4
MichaelT9 <trajkovski.mihail@uklo.edu.mk>
2025-01-08 22:09:33 +0100

    Minor change

--- KnowHubApp.client/src/AuthContext/AuthContext.jsx
+++ KnowHubApp.client/src/AuthContext/AuthContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState, useContext } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
 
   const saveToken = (newToken) => {
     setToken(newToken);
